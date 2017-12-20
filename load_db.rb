70ec7a6e5b6e8236c722dbbd59ff67f4e0a1379f
glara <glara@glara.cl>
2017-12-19 21:57:56 -0300

    removes puts from methods

--- load_db.rb
+++ load_db.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+
 require 'aws-sdk'
 require 'colorize'
 require 'dotenv'
@@ -8,17 +9,16 @@ class LoadDb
                 :rootdir,
                 :database,
                 :mysql_cmd,
-                :option_selected,
+                :backup_selected,
                 :local_extension,
                 :amazon_access_key_id,
                 :amazon_secret_access_key
 
-  def initialize
+  def initialize(client)
     (Dotenv.load "#{__dir__}/.env").each { |key, value| send "#{key}=", value }
 
     @s3 = s3
-    @client = read_client
-    @tree = backups_tree
+    @client = client
     @database['{client}'] = @client
 
     system "mkdir #{@rootdir}" unless Dir.exist? @rootdir
@@ -35,23 +35,6 @@ class LoadDb
     !@amazon_access_key_id.nil? && !@amazon_secret_access_key.nil?
   end
 
-  def print_backups
-    puts '=' * 40
-    puts ' Choose an option'
-    puts '-' * 40
-    @tree.each_with_index do |el, index|
-      puts " [#{(index + 1).to_s.rjust(2)}]".blue + " #{el}".green
-    end
-    puts '=' * 40
-  end
-
-  def select_backup
-    print_backups
-    puts ' > Which?: '.green
-    id = $stdin.gets.chomp.to_i until (1...(@tree.length + 1)).cover? id
-    @option_selected = @tree[id - 1]
-  end
-
   def local_backups
     backups = []
     Dir["#{@rootdir}#{@client}*.gz"].each_with_index do |el, _index|
@@ -60,28 +43,25 @@ class LoadDb
     backups
   end
 
-  def backups_tree
+  def local_backup?
+    local_backups.include? @backup_selected
+  end
+
+  def backups
     @bucket = s3.buckets[@bucket]
     tree = bucket.as_tree(prefix: @client)
     tree = tree.children.select(&:leaf?).collect(&:key)
 
     local_backups.each { |local_backup| tree << local_backup }
-    tree << 'exit'
     tree
   end
 
-  def read_client
-    return ARGV[0] unless ARGV[0].nil?
-    puts 'I need a client name, enter any...'
-    $stdin.gets.chomp
-  end
-
   def backup_date
-    @option_selected.match(/(\d{4}\-\d{2}\-\d{2})/)[0].delete('-')
+    @backup_selected.match(/(\d{4}\-\d{2}\-\d{2})/)[0]
   end
 
   def download_url
-    @bucket.objects[@option_selected].url_for(:read, expires: 20 * 60)
+    @bucket.objects[@backup_selected].url_for(:read, expires: 20 * 60)
   end
 
   def file_name_without_extension
@@ -92,29 +72,20 @@ class LoadDb
     "#{file_name_without_extension}.#{extension}"
   end
 
+  def backup_exists?
+    File.exist? file_name(@local_extension)
+  end
+
   def download
-    file = file_name @local_extension
-    file_exists = false
-
-    if File.exist? file
-      file_exists = true
-      puts 'Database exists, download again ? (y/n)'
-      download_again = $stdin.gets.chomp
-    end
-
-    return false unless !file_exists || download_again == 'y'
-    remove_sql_file
+    file = file_name(@local_extension)
     system "wget '#{download_url}' -O #{file}"
   end
 
   def extract_database
-    puts 'Extracting database...'.blue
     system "pv #{file_name @local_extension} | gunzip > #{file_name 'sql'}"
   end
 
   def import_database
-    puts 'Importing database...'.green
-
     system "perl -pi -w -e \'s/ROW_FORMAT=FIXED//g;\' #{file_name 'sql'}"
     system "#{@mysql_cmd} -e 'DROP DATABASE IF EXISTS `#{@database}`'"
     system "#{@mysql_cmd} -e 'CREATE DATABASE `#{@database}`'"
@@ -126,9 +97,41 @@ class LoadDb
   end
 end
 
-load_db = LoadDb.new
-load_db.select_backup
-load_db.download
-load_db.extract_database
-load_db.import_database
-load_db.remove_sql_file
+client = ARGV[0]
+if client.nil?
+  puts 'I need a client name, enter any... '
+  client = $stdin.gets.chomp
+end
+
+ldb = LoadDb.new client
+backups = ldb.backups
+
+exit if backups.empty?
+
+puts '=' * 64
+puts ' Choose an option'
+puts '-' * 64
+backups.each_with_index do |backup, index|
+  print "[#{(index + 1).to_s.rjust(2)}] ".blue
+  print backup.green
+  print "\n"
+end
+puts '=' * 64
+puts ' > Which ?'.yellow
+
+backup_selected = $stdin.gets.chomp.to_i
+ldb.backup_selected = backups[backup_selected - 1]
+
+answer = 'y'
+if ldb.backup_exists? && !ldb.local_backup?
+  puts 'Download again ? (y/n)'
+  answer = $stdin.gets.chomp
+end
+ldb.download if answer == 'y' && !ldb.local_backup?
+
+puts 'Extracting database...'.blue
+ldb.extract_database
+puts 'Importing database...'.green
+ldb.import_database
+ldb.remove_sql_file
+puts 'Done :)'.green
